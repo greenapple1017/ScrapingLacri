08c556413636c81d712ef8277c9f825d7b52c5b5
UN YONG JEONG <greenapple@UNs-MacBook-Air.local>
2023-03-21 17:20:25 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+url = 'https://orbi.kr/search?type=imin&q=2'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    ul = soup.select_one('ul.post-list')
+    titles = ul.select('li > div > p > a')
+    with open("post", 'w') as f:
+        for title in titles:
+            f.write(title.get_text())
+else:
+    print(response.status_code)
